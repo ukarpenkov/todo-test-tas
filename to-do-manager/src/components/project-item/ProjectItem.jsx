5563c8c3503d2ce9ac29d13dbb3c332f65128911
ukarpenkov <truloveu@gmail.com>
2022-11-27 22:17:11 +0300

    add new project, and  add checked for projects

--- to-do-manager/src/components/project-item/ProjectItem.jsx
+++ to-do-manager/src/components/project-item/ProjectItem.jsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const ProjectItem = ({ name, done, id }) => {
+    const [checked, setChecked] = useState(done)
+    const cls = ['projects-item']
+    if (checked) {
+        cls.push('completed')
+    }
     return (
-        <li key={id} className='projects-item'>
-            <input className='projects-item__checkbox' type="checkbox" defaultChecked={done} />
+        <li key={id} className={cls.join(' ')}>
+            <input className='projects-item__checkbox'
+                type="checkbox"
+                checked={checked}
+                onChange={() => setChecked(!checked)} />
             <span className='projects-item__project-name'>{name}</span>
         </li>
     )
--- to-do-manager/src/pages/projects/ProjectsPage.jsx
+++ to-do-manager/src/pages/projects/ProjectsPage.jsx
@@ -1,13 +1,27 @@
-import React from 'react'
-import CreateNewProjectInput from '../../components/project-item/CreateNewProjectInput'
+import React, { useState } from 'react'
 import ProjectItem from '../../components/project-item/ProjectItem'
 
 const ProjectsPage = (props) => {
 
-    let projects = [{ name: 'Проект 1', done: false, id: 1 }, { name: 'Проект 2', done: true, id: 2 }]
+    const [projects, setProjects] = useState([{ name: 'Проект 1', done: false, id: 1 }, { name: 'Проект 2', done: true, id: 2 }]);
+    const [projectTitle, setProjectTitle] = useState('');
+    const addProject = event => {
+        if (event.key === 'Enter') {
+            setProjects([
+                ...projects,
+                { name: projectTitle, done: false, id: Date.now() }
+            ])
+            setProjectTitle('')
+        }
+    }
+
     return (
         <div className='projects-list'>
-            <CreateNewProjectInput />
+            <div className='create-project'>
+                <input type="text" className='create-project__input' value={projectTitle} onChange={event => setProjectTitle(event.target.value)}
+                    onKeyPress={addProject} />
+                <button className='create-project__btn'>создать проект</button>
+            </div>
             <ul className='projects'>
                 {projects.map(project =>
                 (
@@ -21,3 +35,15 @@ const ProjectsPage = (props) => {
 
 
 export default ProjectsPage
+// const handleProjectStatusChange = id => {
+//     const updatedProjects = projects.map(projects => {
+//         if (projects.id === id) {
+//             return {
+//                 ...projects,
+//                 done: !projects.done
+//             }
+//         }
+//         return projects
+//     })
+
+// }
